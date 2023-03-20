0a62ba4054bb3884e0c31c18e8e5641d42ee8529
James Wang <jwang864@gatech.edu>
2023-03-20 10:08:48 -0400

    application init

---
+++ server/db/conn.js
@@ -0,0 +1,19 @@
+
+const { MongoClient } = require("mongodb");
+const connectionString = process.env.ATLAS_URI;
+const client = new MongoClient(connectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+let dbConnection;
+
+module.exports = {
+  connectToServer: function (callback) {
+    // Implement Database connection
+  },
+
+  getDb: function () {
+    return dbConnection;
+  },
+};
